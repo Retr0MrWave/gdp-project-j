95903abd65f5d6be8f12d56211fc1451c1b32a76
Totem-101 <sundaychau0427@gmail.com>
2026-05-07 23:06:27 +0100

    Upgraded the Score UI.

---
+++ Assets/Scripts/Scorenew.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using TMPro;
+
+public class ScoreNew : MonoBehaviour
+{
+    public PlayerScoring scorer;
+    private TMP_Text myTextComponent;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        myTextComponent = GetComponent<TMP_Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        myTextComponent.SetText(scorer.Score.ToString());
+    }
+}
